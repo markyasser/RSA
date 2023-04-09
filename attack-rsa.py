54944c7a033ebf78cadd82acb92292e58967123e
markyasser <82395903+markyasser@users.noreply.github.com>
2023-04-09 21:51:00 +0200

    attack-rsa loop

--- attack-rsa.py
+++ attack-rsa.py
@@ -1,22 +1,29 @@
 import time
 import rsa
+import numpy as np
+import matplotlib.pyplot as plt
 import generate_key as key
-(e, n), (d, n) = key.generate_key(9)
 
-text = 'hello world'
-encoded = rsa.encode(text)
-encrypted = rsa.group_encrypt(encoded, e, n)
+text = 'h'
+decrypt_time = []
+for i in range(0,10):
+    (e, n), (d, n) = key.generate_key(i)
+    encoded = rsa.encode(text)
+    encrypted = rsa.group_encrypt(encoded, e, n)
+    decrypted = rsa.group_decrypt(encrypted, d, n) # real decryption
+    decoded = rsa.decode(decrypted)
+    #-----------------Attack-----------------
+    start = time.time()
+    d_init = 1
+    while decrypted != rsa.group_decrypt(encrypted, d_init, n):
+        d_init += 1
+    end = time.time()
+    print(f"Execution time: {round(end - start, 4)} seconds , i = {i}")
+    decrypt_time.append(round(end - start, 4))
+    #----------------------------------------
 
-#-----------------Attack-----------------
-decrypted = rsa.group_decrypt(encrypted, d, n)
-decoded = rsa.decode(decrypted)
-start = time.time()
-d_init = d - 100000
-while decrypted != rsa.group_decrypt(encrypted, d_init, n):
-    d_init += 1
-end = time.time()
-print(f"Execution time: {round(end - start, 4)} seconds")
-#----------------------------------------
-
-decrypted = rsa.group_decrypt(encrypted, d, n)
-decoded = rsa.decode(decrypted)
+x = np.linspace(5, 20, 1)
+plt.plot(x,decrypt_time)
+plt.xlabel('Key size')
+plt.ylabel('Decryption Time')
+plt.show()
